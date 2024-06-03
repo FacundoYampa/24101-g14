7a9dd132def72029c350d51ff50786c2e7ed8b2c
Marco Robles <cyberlobo.lider@gmail.com>
2024-06-03 19:46:31 -0300

    añadiendo validaciones de login

--- js/validarlogin.js
+++ js/validarlogin.js
@@ -1,11 +1,26 @@
-<script>
+/* <script>
   function validarFormulario() {
     var email = document.getElementById('email').value;
     var contraseña = document.getElementById('password').value;
 
     if(email === "" || contraseña === "") {
-      alert("Por favor, ingrese usuario y contraseña");
+      alert('Ingrese email y contraseña');
       return false;
     }
   }
-</script>
+</script> */
+
+//Validación para login.html
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+  event.preventDefault(); // Evita que el formulario se envíe automáticamente
+  
+  var email = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
+  
+  // Validar los datos ingresados por el usuario
+  if(email === "" || contraseña === "") {
+    alert ('Ingrese email y contraseña');
+    return false;
+    }
+  }
+);
